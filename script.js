87e118fd005cec035c6a4aa644fe80372298434a
random_rock2002 <73513711+randomrock@users.noreply.github.com>
2021-05-23 12:34:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var audio = document.createElement('audio');
-audio.setAttribute('src','/Car-Animated/sound.mp3');
+audio.setAttribute('src','/Responsive-Lambo/sound.mp3');
 audio.loop = true;
 audio.play();
